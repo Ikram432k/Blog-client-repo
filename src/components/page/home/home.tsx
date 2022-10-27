939f9d841a20654067ab95b88a051bb0db72116a
ikram mohideen <ikram432k@gmail.com>
2022-10-27 21:19:12 +0530

    added home page

---
+++ src/components/page/home/home.tsx
@@ -0,0 +1,27 @@
+import { Link} from "react-router-dom";
+import { useState,useEffect } from "react";
+
+
+
+const Home =()=>{
+    useEffect(()=>{
+
+    },[]);
+
+    const getPost =async()=>{
+
+    }
+    return(
+        <div>
+            <div>
+                <p>
+                    Post your own views and perspectives as blog by signing up <Link to="">here</Link>.
+                </p>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
+export default Home;
