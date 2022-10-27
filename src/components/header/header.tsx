f3963abcc2ccc8dc8147436cab4c7d6c0114b6a3
ikram mohideen <ikram432k@gmail.com>
2022-10-27 21:18:46 +0530

    added header file

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+const Nav =()=>{
+    return(
+        <div>
+            <Link to="/">
+                home
+            </Link>
+            <h2>blog</h2>
+        </div>
+    )
+}
+export default Nav;
