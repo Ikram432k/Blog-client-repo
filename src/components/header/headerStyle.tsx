6b933cbeced6a09ddf28805acf790750cd29f470
ikram mohideen <ikram432k@gmail.com>
2022-10-28 21:41:04 +0530

    created style to header

---
+++ src/components/header/headerStyle.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+export const Div = styled.div`
+display: flex;
+justify-content: space-between;
+align-items: center;
+text-transform: uppercase;
+background-color: rgb(83, 45, 189);
+color:  white;
+padding: 20px;
+    p{
+    font-family: 'Orbitron', sans-serif;
+        font-size: 25px;
+        margin-right: 25px;
+        padding:5px;
+        color:white;
+        background-color: black;
+    }
+
+    `;
