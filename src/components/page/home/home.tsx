faa7158a2c9374e0529dc704a1a5d4ff9dec1bab
ikram mohideen <ikram432k@gmail.com>
2022-10-28 21:42:05 +0530

    styled added to home page

--- src/components/page/home/home.tsx
+++ src/components/page/home/home.tsx
@@ -1,15 +1,24 @@
-import { Link} from "react-router-dom";
+// import { Link} from "react-router-dom";
 import { useState,useEffect } from "react";
-
+import axios from "axios";
+import { Link } from "react-router-dom";
+import { Main,Div} from './homeStyle';
 
 
 const Home =()=>{
     useEffect(()=>{
-
+        getPost();
     },[]);
-
+    const [post,setPost] = useState<any[]>([])
     const getPost =async()=>{
-
+        try{
+        const response = await axios.get('http://localhost:3000/api/posts');
+        const posts = response.data.posts;
+        setPost(posts);
+        console.log(posts);
+        }catch(err){
+            return err;
+        }
     }
     return(
         <div>
@@ -18,9 +27,17 @@ const Home =()=>{
                     Post your own views and perspectives as blog by signing up <Link to="">here</Link>.
                 </p>
             </div>
-            <div>
-
-            </div>
+            <Main>
+                {post.map((obj,i)=>(
+                    <Div key={i}>
+                        <h3>{obj.title}</h3>
+                        <p>{obj.author.username}</p>
+                        <Link to={`/post/${obj._id}`}>
+                            <button>view post</button>
+                        </Link>
+                    </Div>
+                ))}
+            </Main>
         </div>
     )
 }
