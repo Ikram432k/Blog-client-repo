11ffe3aae8ec0ecd08356564e56daedec2ce568e
ikram mohideen <ikram432k@gmail.com>
2022-10-31 17:37:51 +0530

    login and sign up routes added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Nav from "./components/header/header";
 import Home from './components/page/home/home';
 import Post from './components/page/post/post';
 import { createGlobalStyle } from 'styled-components';
+import Login from './components/author/login/login';
+import Signup from './components/author/signup/signup';
 const GlobalStyle = createGlobalStyle`
  body,h1,h2,h3,p {
   margin: 0;
@@ -20,6 +22,8 @@ function App() {
       <Routes>
         <Route path='*' element={<Home/>} />
         <Route path='/post/:id' element={<Post/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<Signup/>}/>
       </Routes>
     </Router>
   );
