315c0503c6d1d3edfa669ae5a6e32d82ec71f55f
ikram mohideen <ikram432k@gmail.com>
2022-11-01 20:10:48 +0530

    dashboard page created

--- src/components/author/dashboard/dashboard.tsx
+++ src/components/author/dashboard/dashboard.tsx
@@ -1,20 +1,101 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+// import { useParams } from "react-router-dom";
+import '../dashboard/dashBoardStyle.scss';
+
 const Dashboard =()=>{
+
+    interface userDetails {
+    username: string,
+    _id: string,
+    posts: Array<string>
+}
+
+useEffect(()=>{
+    setUserDetails();
+},[])
+
+
+const[lsUser,setUser] =useState<userDetails>({
+    posts:[],
+    username:'',
+    _id:''
+});
+useEffect(()=>{
+    getPost();
+// eslint-disable-next-line react-hooks/exhaustive-deps
+},[lsUser]);
+
+const[userToken,setToken] =useState<string>('');
+
+const[allPosts,setAllPost] = useState<any[]>([]); 
+
+    const setUserDetails=()=>{
+        const currentUser = JSON.parse(localStorage.getItem('user')! );
+        const token = localStorage.getItem('token')!;
+        setUser({...currentUser});
+        setToken(token);
+        console.log(lsUser,userToken);
+    }
+
+    // const deletePost=async()=>{
+
+    //     try{
+    //     const response = await axios.post(`http://localhost:3000/api/posts/:postid/delete`);
+    //     }catch(err){
+    //         return err;
+    //     }
+    // }
+
+
+    const getPost =async()=>{
+        const arr = lsUser.posts;
+        for(let i=0;i<arr.length;i++){
+            try{
+                const response = await axios.get(`http://localhost:3000/api/posts/${arr[i]}`);
+                console.log(response.data.post);
+                let temp = allPosts;
+                temp.push(response.data.post);
+                setAllPost([...temp])
+                console.log(allPosts);
+                }catch(err){
+                    return err;
+                }
+        }
+    }
+
     return(
-        <div>
-            <div>
-                <h2>ikram mohideen</h2>
-                <p>total no of posts: 7</p>
-            </div>
-            <div>
-                <div>
-                    <h3>title of the post</h3>
-                    <p>Motivation only exists as a noun. But discipline has a verb form. In other words, discipline is something you do, and choose to do; motivation is a thing that can come and go, which you cannot choose to do or control. If there’s something in your life you want to change — large or small — utilize your newfound motivation to decide on a new action you’re going to take daily that will help you to change that thing.</p>
+        <div className="dashContent">
+            <div className='dashContainer'>
+                <div className="dashHead">
+                    <h2>User : {lsUser.username}</h2>
+                    <p>Total number of posts : {(lsUser.posts).length}</p>
                 </div>
-                <div>
-                    <div className="contentCom">
-                        <p className="name">ikarm says ,</p>
-                        <p>"Motivation will fire the engine… at first. Discipline will keep you going when you think it’s out of gas."</p>
-                    </div>
+                <div className="dashMain">
+                {allPosts.map((obj,i)=>(
+                    <div className='dashCard-in' key={i}>
+                        <div className='dashCard'>
+                            <div className='cardtitle'>
+                                <h3>Title : {obj.title}</h3>
+                                <p><i className='far fa-comment'></i> {obj.comments.length}</p>
+                            </div>
+                            <div>
+                                {/* <i className='far fa-edit'></i> */}
+                                <button>
+                                    <i className='fas fa-trash'></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div className='dashbtn'>
+                            <Link to=''>
+                                <button>
+                                    view Post
+                                </button>  
+                            </Link>
+                        </div>
+                </div>
+                    ))}
                 </div>
             </div>
         </div>
