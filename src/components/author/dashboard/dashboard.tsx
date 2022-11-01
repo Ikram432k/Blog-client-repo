3d38c11f52eba092f2ade132a40ea105885d4d55
ikram mohideen <ikram432k@gmail.com>
2022-11-01 07:52:37 +0530

    created dashboard

---
+++ src/components/author/dashboard/dashboard.tsx
@@ -0,0 +1,23 @@
+const Dashboard =()=>{
+    return(
+        <div>
+            <div>
+                <h2>ikram mohideen</h2>
+                <p>total no of posts: 7</p>
+            </div>
+            <div>
+                <div>
+                    <h3>title of the post</h3>
+                    <p>Motivation only exists as a noun. But discipline has a verb form. In other words, discipline is something you do, and choose to do; motivation is a thing that can come and go, which you cannot choose to do or control. If there’s something in your life you want to change — large or small — utilize your newfound motivation to decide on a new action you’re going to take daily that will help you to change that thing.</p>
+                </div>
+                <div>
+                    <div className="contentCom">
+                        <p className="name">ikarm says ,</p>
+                        <p>"Motivation will fire the engine… at first. Discipline will keep you going when you think it’s out of gas."</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Dashboard;
