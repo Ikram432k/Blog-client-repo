25299f1a891683237923abe9af6ef0e46036b24b
ikram mohideen <ikram432k@gmail.com>
2022-11-05 15:45:52 +0530

    header page updated

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,43 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import "./headerStyle.scss";
+
 const Nav =()=>{
+    const[visible,setVisible]=useState<boolean>(true)
+    const navigate = useNavigate();
     const navStyle={
         color:"white",
         textDecoration: 'none'
         };
 
-    const logout=()=>{
-        localStorage.clear();
+    const login=()=>{
+        setVisible(false)
+        navigate('/login')
     }
+    const logout=()=>{
+        // setSettrue(view.show)
+        localStorage.clear();
+        setVisible(true)
+        navigate('/')
+    }
+
     return(
-        <div className="divd">
-            <Link style={navStyle} to="/">
-            <h2>blog</h2>
-            </Link>
-            <Link to="/">
-            <button onClick={logout}>
+        <div className="header-div">
+            <div className="head-title">
+                <Link style={navStyle} to="/">
+                <h2>blog</h2>
+                </Link>
+            </div>
+            <div className="header-btn">
+            <button className="header-icon" onClick={logout} style={{display: !visible ? 'block' : 'none' }}>
             <i className='fas fa-sign-out-alt'></i>
+            <p>logout</p>
             </button>
-            </Link>
+            <button className="header-icon" onClick={login} style={{display: visible ? 'block':'none' }}>
+            <i className='fas fa-sign-in-alt'></i>
+            <p>login</p>
+            </button>
+            </div>
         </div>
     )
 }
