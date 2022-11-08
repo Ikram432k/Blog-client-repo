5f7528bafefbc9c0f590f9471e45b1a1d958f681
ikram mohideen <ikram432k@gmail.com>
2022-11-08 22:27:57 +0530

    updated header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,24 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "./headerStyle.scss";
+interface propsUser{
+    user:{
+        _id:string,
+        username:string
+    }
+}
+const Nav =({user}:propsUser)=>{
+    const[visible,setVisible]=useState<boolean>(false);
+    const[userName,setUserName]=useState<any>('');
+    useEffect(()=>{
+        if(localStorage.getItem('userName')){
+            setUserName(localStorage.getItem('userName'));
+            setVisible(true);
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[user])
+    // useEffect(()=>{
 
-const Nav =()=>{
-    const[visible,setVisible]=useState<boolean>(true)
+    //     if(user.username){
+    //         setUserName(user.username);
+    //         setVisible(true);
+    //     }
+    // // eslint-disable-next-line react-hooks/exhaustive-deps
+    // },[user])
     const navigate = useNavigate();
     const navStyle={
         color:"white",
         textDecoration: 'none'
         };
 
-    const login=()=>{
-        setVisible(false)
-        navigate('/login')
-    }
     const logout=()=>{
-        // setSettrue(view.show)
         localStorage.clear();
-        setVisible(true)
-        navigate('/')
+        // setVisible(true)
+        setVisible(false);
+        setUserName('');
+        navigate('*')
+
+    }
+
+    const takToProfile=()=>{
+        navigate(`/dashboard/${user._id}/${user.username}`);
     }
 
     return(
@@ -28,15 +51,17 @@ const Nav =()=>{
                 <h2>blog</h2>
                 </Link>
             </div>
-            <div className="header-btn">
-            <button className="header-icon" onClick={logout} style={{display: !visible ? 'block' : 'none' }}>
-            <i className='fas fa-sign-out-alt'></i>
-            <p>logout</p>
-            </button>
-            <button className="header-icon" onClick={login} style={{display: visible ? 'block':'none' }}>
-            <i className='fas fa-sign-in-alt'></i>
-            <p>login</p>
-            </button>
+            <div className="header-btn" style={{display: visible ? 'flex' : 'none' }}>
+                <button className="header-user-icon" onClick={takToProfile}>
+                    <i className="fa fa-user-circle-o"></i>
+                </button>
+                    <div className="user">
+                        <p>{userName}</p>
+                        <button className="header-icon" onClick={logout} >
+                            <i className='fas fa-sign-out-alt'></i>
+                            <p>logout</p>
+                        </button>
+                    </div>
             </div>
         </div>
     )
